2f87a3e07cf782bdc05456a3f68e5a5a28e39d02
高权 <mygao666@gmail.com>
2019-07-20 14:36:24 +0800

    :sparkles: 新增tabs

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,33 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigration',
-          onPressed: () => debugPrint("menu"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.local_activity)),
+              Tab(icon: Icon(Icons.directions_bike))
+            ]
+          ),    
+            
         ),
-        title: Text('Hello, Flutter'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: () => debugPrint("search"),
-          )
-        ],
-        elevation: 30.0,
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128, color: Colors.black12),
+            Icon(Icons.local_activity, size: 128, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128, color: Colors.black12)
+          ],
+        ),
       ),
-      body: null,
-    );
+      );
   }
 } 
 
